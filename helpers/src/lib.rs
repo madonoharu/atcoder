8f2e2d5f70c66695783a1418d946a86799e5d662
madonoharu <haruki.kuramoto@labbase.co.jp>
2025-12-27 22:55:24 +0900

    update

--- helpers/src/lib.rs
+++ helpers/src/lib.rs
@@ -2,6 +2,7 @@ pub use internals::*;
 
 pub mod prelude {
     pub use itertools::Itertools;
+    pub use num::Integer;
     pub use proconio::marker::*;
     pub use rand::prelude::*;
     pub use std::collections::*;
