2afa79a7238c5826a56074aeb66b6c4618b8254c
madonoharu <haruki.kuramoto@labbase.co.jp>
2025-12-31 03:11:43 +0900

    bump

--- clip.sh
+++ clip.sh
@@ -7,23 +7,10 @@ else
 	bin="$name-$1"
 fi
 
-cargo metadata --no-deps --format-version=1 |
-	jq -r --arg bin $bin '.packages[] | .targets[] | select(.name == $bin) | .src_path' |
-	read src_path
+src=$(cargo equip --exclude-atcoder-crates --exclude internals --minify libs --no-check --bin $bin)
 
-if [[ ! -s $src_path ]]; then
-	echo "Error!" >&2
-	exit 1
-fi
-
-src=$(cat $src_path)
 from="#[helpers::main]"
 to="#[argio::argio(input = proconio::input)]\n#[proconio::fastout]"
 src=${src/$from/$to}
 
-lib=$(cat 'helpers/src/lib.rs')
-lib=${lib##'pub use internals::*;'}
-lib=${lib#"${lib%%[!"$IFS"]*}"}
-lib="mod helpers {$lib}"
-
-printf "$src\n\n$lib" | rustfmt | pbcopy
+echo $src | pbcopy
